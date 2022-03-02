4b38ba5f2ab06ac7cf7e88f0b852c8171fcf4dc9
fredrvaa <fredrvaa@stud.ntnu.no>
2022-03-02 10:17:53 +0100

    Use convolutional layers

--- autoencoders/autoencoder.py
+++ autoencoders/autoencoder.py
@@ -1,20 +1,25 @@
-from keras import Sequential, layers, Model
-
+from tensorflow import keras
+from keras import Sequential, layers, losses
+from keras.losses import binary_crossentropy
 
 class Autoencoder:
     def __init__(self):
         self.encoder = Sequential([
-            layers.Input(shape=(28, 28, 1)),
-            layers.Flatten(),
-            layers.Dense(16, activation='relu')
-        ], name='encoder')
+            layers.Conv2D(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', input_shape=(28, 28, 1)),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(16, kernel_size=(2, 2), strides=(2, 2), activation='relu'),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+        ])
         self.decoder = Sequential([
+            layers.Conv2DTranspose(16, kernel_size=(2, 2), strides=(2, 2)),
+            layers.Flatten(),
             layers.Dense(784, activation='relu'),
             layers.Reshape((28, 28, 1))
-        ], name='decoder')
+        ])
+
         self.model = Sequential()
         self.model.add(self.encoder)
         self.model.add(self.decoder)
-        self.model.compile(loss='mse', optimizer='Adam')
+        self.model.compile(loss=losses.binary_crossentropy, optimizer=keras.optimizers.Adam(lr=.0001))
         self.model.build((None, 28, 28, 1))
         self.model.summary()
--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-from enum import auto
 import matplotlib.pyplot as plt
 from autoencoders.autoencoder import Autoencoder
 from utils.stacked_mnist import StackedMNISTData, DataMode
+from utils.verification_net import VerificationNet
 
-gen = StackedMNISTData(
-    mode=DataMode.MONO_BINARY_COMPLETE,
-    default_batch_size=9)
+gen = StackedMNISTData(mode=DataMode.MONO_BINARY_COMPLETE)
 x_train, y_train = gen.get_full_data_set(training=True)
 x_test, y_test = gen.get_full_data_set(training=False)
 print(x_train.shape, y_train.shape)
 print(x_test.shape, y_test.shape)
 
 autoencoder = Autoencoder()
-autoencoder.model.fit(x_train, x_train, epochs=3)
+autoencoder.model.fit(x_train, x_train, epochs=20, batch_size=256)
 
+# Example
 ex = x_test[10]
 plt.imshow(ex, cmap='gray')
 plt.show()
-encoded = autoencoder.encoder.predict(ex.reshape(-1, 28, 28, 1))[0]
-plt.imshow(encoded.reshape(4, 4), cmap='gray')
-plt.show()
+# encoded = autoencoder.encoder.predict(ex.reshape(-1, 28, 28, 1))[0]
+# plt.imshow(encoded.reshape(3, 3), cmap='gray')
+# plt.show()
 decoded = autoencoder.model.predict(ex.reshape(-1, 28, 28, 1))[0]
 plt.imshow(decoded, cmap='gray')
 plt.show()
+print(x_test.shape)
+x_pred = autoencoder.model.predict(x_test)
+
+# Train verification net
+net = VerificationNet(force_learn=False)
+net.train(generator=gen, epochs=5)
+# I have no data generator (VAE or whatever) here, so just use a sampled set
+cov = net.check_class_coverage(data=x_pred, tolerance=.8)
+pred, acc = net.check_predictability(data=x_pred, correct_labels=y_test)
+print(f"Coverage: {100*cov:.2f}%")
+print(f"Predictability: {100*pred:.2f}%")
+print(f"Accuracy: {100 * acc:.2f}%")
